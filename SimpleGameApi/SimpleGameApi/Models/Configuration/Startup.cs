e062a1e3e5bdf04d84b1be57a035ac3f04e1b35e
andreleaos <andreleaos@gmail.com>
2024-11-27 00:03:11 -0300

    Configuracoes da app para bd

---
+++ SimpleGameApi/SimpleGameApi/Models/Configuration/Startup.cs
@@ -0,0 +1,38 @@
+﻿
+namespace SimpleGameApi.Models.Configuration;
+
+public static class Startup
+{
+    public static void Configure(IConfiguration configuration, IServiceCollection services)
+    {
+        ConfigureDatabase(configuration, services);
+        ConfigureDependencies(services);
+    }
+
+    private static void ConfigureDatabase(IConfiguration configuration, IServiceCollection services)
+    {
+        var connStr = configuration.GetConnectionString("JogosDB") ??
+            throw new ArgumentNullException("Connection String não localizada");
+
+        // incluir configuração para o contexto do meu EF Core
+    }
+
+    private static void ConfigureDependencies(IServiceCollection services)
+    {
+        ConfigureRepositories(services);
+        ConfigureServices(services);
+
+    }
+
+    private static void ConfigureRepositories(IServiceCollection services)
+    {
+        // Incluir dependencias de repositorios
+    }
+
+    private static void ConfigureServices(IServiceCollection services)
+    {
+        // Incluir dependencias de servicos
+    }
+
+
+}
--- SimpleGameApi/SimpleGameApi/Program.cs
+++ SimpleGameApi/SimpleGameApi/Program.cs
@@ -1,6 +1,12 @@
+using SimpleGameApi.Models.Configuration;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+var config = builder.Configuration;
+var services = builder.Services;
+
+Startup.Configure(config, services);
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
