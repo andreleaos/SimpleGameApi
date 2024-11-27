3707a1e9055fffd9db602278cf7cbc8e017a4e43
andreleaos <andreleaos@gmail.com>
2024-11-26 21:34:22 -0300

    primeira interface generica

---
+++ SimpleGameApi/SimpleGameApi/Models/Domain/Contracts/Base/IGenericRepository.cs
@@ -0,0 +1,10 @@
+﻿namespace SimpleGameApi.Models.Domain.Contracts.Base;
+public interface IGenericRepository<T, Y>
+    where T : class
+{
+    void Add(T entity);
+    bool Update(T entity);
+    bool Delete(Y id);
+    T Get(Y id);
+    List<T> GetAll();
+}
